a1c5d3f25611b32ec0210e97477ba46b4f61e6a4
SpiderBuh <ijesbgujdbuheagfvues@gmail.com>
2024-02-02 21:25:13 +0200

    Add project files.

---
+++ ScpPcSkeleSwap/Plugin.cs
@@ -0,0 +1,17 @@
+﻿using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+using PluginAPI.Events;
+
+namespace ScpPcSkeleSwap
+{
+    public class Plugin
+    {
+        [PluginEntryPoint("Abonedonware", "0.0.1", "Removes 079 and replaces his role of freeing SCPs with a reworked 3114", "SpiderBuh")]
+        public void OnPluginStart()
+        {
+            Log.Info($"Plugin is loading...");
+            EventManager.RegisterEvents<Events>(this);
+        }
+
+    }
+}
