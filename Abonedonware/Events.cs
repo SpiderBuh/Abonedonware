736562684e2ec20b2bc8e718b9aa14e0ccd02600
SpiderBuh <ijesbgujdbuheagfvues@gmail.com>
2024-02-03 15:05:55 +0200

    File space name changed and basic spawning logic

---
+++ Abonedonware/Events.cs
@@ -0,0 +1,17 @@
+﻿using PluginAPI.Core.Attributes;
+using PluginAPI.Enums;
+using PluginAPI.Events;
+
+namespace Abonedonware
+{
+    public class Events
+    {
+        [PluginEvent(ServerEventType.PlayerSpawn)]
+        public void PlayerSpawn(PlayerSpawnEvent args)
+        {
+            var plr = args.Player;
+            if (plr.Role == PlayerRoles.RoleTypeId.Scp079)
+                plr.SetRole(PlayerRoles.RoleTypeId.Scp3114, PlayerRoles.RoleChangeReason.RoundStart);
+        }
+    }
+}
--- Abonedonware/Plugin.cs
+++ Abonedonware/Plugin.cs
@@ -2,11 +2,11 @@
 using PluginAPI.Core.Attributes;
 using PluginAPI.Events;
 
-namespace ScpPcSkeleSwap
+namespace Abonedonware
 {
     public class Plugin
     {
-        [PluginEntryPoint("Abonedonware", "0.0.1", "Removes 079 and replaces his role of freeing SCPs with a reworked 3114", "SpiderBuh")]
+        [PluginEntryPoint("Abonedonware", "0.0.2", "Removes 079 and replaces his role of freeing SCPs with a reworked 3114", "SpiderBuh")]
         public void OnPluginStart()
         {
             Log.Info($"Plugin is loading...");
